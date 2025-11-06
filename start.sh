180e2211c7c549d06b86084544fcf059986b72da
lakshya <lakshyabanjare4@gmail.com>
2025-11-07 00:36:28 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,3 @@
+wget https://fill-data.papermc.io/v1/objects/c4fdb280847a142761a4e3251a4c8be4342c7139f4d91e042960a48463a4b5fd/paper-1.21.10-105.jar
+
+java -Xms2G -Xmx4G -jar paper-1.21.10-105.jar nogui
